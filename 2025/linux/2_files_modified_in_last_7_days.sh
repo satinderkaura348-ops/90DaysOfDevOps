ed66125b2c2d7afef927af0d9f037e57e84cae4a
satinderkaura348-ops <satinderkaura348@gmail.com>
2025-09-27 08:21:47 +1000

    removing from master branch as added to linux branch

--- 2025/linux/2_files_modified_in_last_7_days.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# This script Use find to list all files modified in the last 7 days.
-
-
-
--- 2025/linux/3_error_and_warning_logs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#
-# This script extracts and displays only ERROR and WARNING logs from Linux_2k.log.
-
-
